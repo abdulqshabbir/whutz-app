78766dcd56c71dcc869f0e8cdc93f1d4824220da
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-08-12 15:56:34 +0800

    fix: :bug: env variables loading

--- src/env.mjs
+++ src/env.mjs
@@ -1,12 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 import dotenv from "dotenv"
-
-if (process.env.NODE_ENV === "development") {
-  dotenv.config({ path: ".env.local" })
-} else {
-  dotenv.config({ path: ".env.production" })
-}
+dotenv.config()
 
 export const env = createEnv({
   /**
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,8 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { sqliteTable, AnySQLiteColumn, integer, text } from "drizzle-orm/sqlite-core"
+import { sql } from "drizzle-orm"
+
 
 export const test = sqliteTable("test", {
-  id: integer("id").primaryKey().notNull(),
-  text: text("text").notNull(),
-})
+	id: integer("id").primaryKey().notNull(),
+	text: text("text").notNull(),
+});
