e094a78b922f192d95df77f65b8cd47aa6b3bd2f
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-08-19 23:59:11 +0800

    refactor: :recycle: remove next-auth comments which disabled ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-/*
-  There seems to be a bug in the types for the drizzle-adapter and next-auth
-  which is why we have to use `any` in some places.
-*/
-
 import { authOptions } from "@/server/auth"
 import NextAuth from "next-auth"
 
