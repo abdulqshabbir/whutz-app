01e8ea5065f649e7ab14e1dcbab95c133bb9e308
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-09-10 14:56:18 +0800

    fix: :bug: large device friend avatar placement

--- src/components/AccountBar.tsx
+++ src/components/AccountBar.tsx
@@ -3,7 +3,7 @@ import { AccountBarDropdown } from "./AccountDropdown"
 export function AccountBar() {
   return (
     <div
-      className={`no-scrollbar fixed left-0 top-0 z-10 flex min-h-[60px] w-full flex-row items-center gap-4 overflow-x-auto bg-slate-400 px-4 py-2 sm:static sm:block sm:h-full sm:max-w-[75px] sm:flex-col sm:bg-gray-200 sm:px-2 sm:py-0`}
+      className={`no-scrollbar fixed left-0 top-0 z-10 flex min-h-[60px] w-full flex-row items-center gap-4 overflow-x-auto bg-slate-400 px-4 py-2 sm:static sm:flex sm:h-full sm:max-w-[75px] sm:flex-col sm:bg-gray-200 sm:px-2 sm:py-0`}
     >
       <AccountBarDropdown />
     </div>
