7e3afcf0cfb4c2b9143558de5c305d2cee68b9b5
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-09-18 23:22:59 +0800

    add logs for new pusher client instances

--- src/hooks/usePusher.tsx
+++ src/hooks/usePusher.tsx
@@ -1,6 +1,8 @@
 import { createContext, useContext } from "react"
 import { env } from "@/env.mjs"
 import Pusher from "pusher-js"
+import { logger } from "@/utils/logger"
+import { useUser } from "./useUser"
 
 const PusherContext = createContext<null | Pusher>(null)
 let pusherCached: null | Pusher = null
@@ -14,9 +16,16 @@ function usePusher() {
 }
 
 function PusherProvider({ children }: { children: React.ReactNode }) {
+  const { email } = useUser()
   if (!pusherCached) {
     pusherCached = new Pusher(env.NEXT_PUBLIC_PUSHER_KEY, {
       cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    })
+    logger({
+      message: "new pusher client created",
+      level: "warn",
+      data: null,
+      email,
     })
   }
 
